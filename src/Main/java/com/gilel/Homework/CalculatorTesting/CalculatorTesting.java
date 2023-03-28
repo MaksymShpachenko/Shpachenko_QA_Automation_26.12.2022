f4966a6e4acd0d99f4bf62170ecfd5544014fbc1
Maksym Shpachenko <maris.latvija@inbox.lv>
2023-03-28 17:50:51 +0300

    Homework_19, Calculator testing (division testing changed to stream of arguments)

--- src/Main/java/com/gilel/Homework/CalculatorTesting/CalculatorTesting.java
+++ src/Main/java/com/gilel/Homework/CalculatorTesting/CalculatorTesting.java
@@ -5,7 +5,11 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 public class CalculatorTesting {
     private static Calculator calculator;
@@ -19,28 +23,35 @@ public class CalculatorTesting {
     @DisplayName("Addition testing 2 + 2")
     public void testAddition(){
         int result = calculator.addition(2,2);
-        Assertions.assertEquals(4, result, "Failed!" + result);
+        Assertions.assertEquals(4, result, "Failed! " + result);
     }
 
     @Test
     @DisplayName("Subtraction testing 4 - 2")
     public void testSubtraction(){
         int result = calculator.subtraction(4,2);
-        Assertions.assertEquals(2, result, "Failed!" + result);
+        Assertions.assertEquals(2, result, "Failed! " + result);
     }
 
     @Test
     @DisplayName("Multiply testing 3 * 3")
     public void testMultiply(){
         int result = calculator.multiply(3,3);
-        Assertions.assertEquals(9, result, "Failed!" + result);
+        Assertions.assertEquals(9, result, "Failed! " + result);
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {3, 6, 9})
+    @MethodSource("valueProvider")
     @DisplayName("Division testing 3 / 3, 6 / 3 , 9 / 3")
-    public void testDivision(int a){
-        int result = calculator.division(a,3);
-        Assertions.assertFalse(result == 15, "Failed!" + result);
+    public void testDivision(int a, int b, int expected){
+        int result = calculator.division(a, b);
+        Assertions.assertEquals(result, expected, "Failed! " + result);
+    }
+
+    public static Stream<Arguments> valueProvider() {
+        return Stream.of(
+                Arguments.arguments(3, 3, 1),
+                Arguments.arguments(6, 3, 2),
+                Arguments.arguments(9, 3, 3));
     }
 }
